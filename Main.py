89a493b848a6ed3e0bbbc6d6b90cb5e6fd69ccc4
ThreePointFadeAwayDunk <86195687+ThreePointFadeAwayDunk@users.noreply.github.com>
2021-06-25 13:27:08 -0400

    Adding main file

---
+++ Main.py
@@ -0,0 +1,20 @@
+from Config import *
+from pytickersymbols import PyTickerSymbols
+
+def main():
+    nasdaq_100_tickers = []
+    stock_data = PyTickerSymbols()
+    nasdaq_stocks = list(stock_data.get_stocks_by_index('NASDAQ 100'))
+    for stocks in nasdaq_stocks:
+        nasdaq_100_tickers.append(stocks["symbol"])
+    
+    nasdaq_100_tickers.remove("CTRP")
+    first_10 = nasdaq_100_tickers[20:35]
+    long_strategy = Long_pull_strategy(first_10)
+    long_strategy.create_sell_order()
+    # long_strategy.generate_satisfied_tickers()
+    # print(long_strategy.get_satisfied_ticker())
+    # long_strategy.create_buy_order()
+
+if __name__ == '__main__':
+    main()
